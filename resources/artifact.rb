baf3074acdb29444992bc903c379608f735643a6
Mariani Lucas <lucas.mariani@supervielle.com.ar>
2020-03-02 12:29:31 -0300

    Fix artifact resource download_url

--- resources/artifact.rb
+++ resources/artifact.rb
@@ -6,7 +6,7 @@ property :name, kind_of: String, name_attribute: true
 property :version, kind_of: String, required: true
 property :url, kind_of: String, required: true
 property :phase, kind_of: String, required: true, default: nil
-property :context, kind_of: String, required: true
+property :context, kind_of: String, required: true, default: nil
 property :extension, kind_of: String, required: true
 property :protocol, kind_of: String, required: true, default: 'https'
 property :path, kind_of: String, required: true default: "#{Chef::Config[:file_cache_path]}/#{new_resource.name}"
@@ -21,8 +21,15 @@ action_class do
   end
 
   def download_url
-    auth = "#{new_resource.user}:#{new_resource.password}"  
-    download_url = "#{new_resource.protocol}://#{auth}@#{new_resource.url}/#{new_resource.phase}/#{new_resource.context}/#{filename}"
+    auth = "#{new_resource.user}:#{new_resource.password}"
+    download_url = "#{new_resource.protocol}://#{auth}@#{new_resource.url}"
+    unless phase.nil?
+      download_url += "/#{new_resource.phase}"
+    end
+    unless context.nil?
+      download_url += "/#{new_resource.context}"
+    end
+    download_url += "/#{filename}"
   end
 
   def file_exist?
@@ -36,7 +43,7 @@ action_class do
 end
 
 action :create do
-  file "#{new_resource.path}.#{filnamee}" do
+  file "#{new_resource.path}.#{filname}" do
     action :delete
     only_if { file_exist? && overwrite }
   end
@@ -49,6 +56,5 @@ action :create do
   remote_file "#{new_resource.path}.#{filnamee}" do
     source download_url
     sensitive true
-    not_if { file_exist? && !overwrite }
   end
 end
